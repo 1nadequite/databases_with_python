df592604aada940f9f73a559fbde6b32164f66e7
1nadequite <yaretsaa@gmail.com>
2016-07-26 22:07:55 +0300

    retrieving geodata

---
+++ w5/where.js
@@ -0,0 +1,9 @@
+myData = [
+[42.340082,-71.0894884, 'Northeastern, Boston, MA 02115, USA'],
+[40.7399972,-74.1775311, 'Bradley Hall, 110 Warren St, Newark, NJ 07102, USA'],
+[32.778949,35.019648, 'Technion/ Sports Building, Haifa'],
+[33.1561058,131.826132, 'Japan, 〒875-0002 Ōita-ken, Usuki-shi, Shitanoe, 1232−2 ＵＭＤ'],
+[42.4036848,-71.120482, 'South Hall Tufts University, 30 Lower Campus Rd, Somerville, MA 02144, USA'],
+[-38.1518106,145.1345412, 'Monash University, Frankston VIC 3199, Australia'],
+[53.2948229,69.4047872, 'Kokshetau 020000, Kazakhstan']
+];
